5acd6d18e7085e9ba6563caec45ab7cebc1e426e
willieny <wbm@ic.ufal.br>
2020-07-10 00:37:39 -0300

    Moving pieces

--- src/boardgame/Board.java
+++ src/boardgame/Board.java
@@ -44,7 +44,19 @@ public class Board {
 		}
 		pieces[position.getRow()][position.getColumn()] = piece;
 		piece.position = position;
-		
+	}
+	
+	public Piece removePiece(Position position) {
+		if(!positionExists(position)) {
+			throw new BoardException("Position not on the board.");
+		}
+		if(piece(position) == null) {
+			return null;
+		}
+		Piece aux = piece(position);
+		aux.position = null;
+		pieces[position.getRow()][position.getColumn()] = null;
+		return aux;
 	}
 	
 	private boolean positionExists(int row, int column) {
@@ -61,4 +73,5 @@ public class Board {
 		}
 		return piece(position) != null;
 	}
+	
 }
